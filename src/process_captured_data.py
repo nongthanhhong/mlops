5d69a2de777de95fc2b9f0d9a3d40c37bef52427
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-06-28 23:38:43 +0700

    .

--- src/process_captured_data.py
+++ src/process_captured_data.py
@@ -195,9 +195,10 @@ def label_captured_data(prob_config: ProblemConfig):
         captured_x = pd.concat([captured_x, captured_data])
         os.remove(file_path)
     
+    captured_x = captured_x.drop_duplicates()
     captured_x.to_parquet(prob_config.captured_data_dir / "total_data.parquet")
-    captured_x = captured_x[captured_x['is_drift']==1].drop_duplicates() #just use drift
-    # captured_x = captured_x.drop_duplicates() #use all request data
+
+    captured_x = captured_x[captured_x['is_drift']==1] #just use drift
     
     logging.info('Preprocessing captured data....')
     if prob_config.prob_id == 'prob-1':
