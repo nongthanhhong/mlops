eafac7e4bb08b874c15f6cb379fac1782209059e
thanhhong <64731516+nongthanhhong@users.noreply.github.com>
2023-06-29 22:49:53 +0700

    .

--- src/process_captured_data.py
+++ src/process_captured_data.py
@@ -3,29 +3,30 @@ import yaml
 import json
 import time
 import logging
+import joblib
 import argparse
 import numpy as np
-from tqdm import tqdm
-from utils import *
 import pandas as pd
-from scipy.spatial.distance import cdist
+from utils import *
+from tqdm import tqdm
 from sklearn import metrics
-from sklearn.cluster import DBSCAN, KMeans, MiniBatchKMeans
-import joblib
-from problem_config import ProblemConfig, ProblemConst, get_prob_config
+from scipy.spatial.distance import cdist
+from sklearn.preprocessing import StandardScaler
 from data_engineering import DataAnalyzer, FeatureExtractor
+from sklearn.cluster import DBSCAN, KMeans, MiniBatchKMeans
+from problem_config import ProblemConfig, ProblemConst, get_prob_config
 from raw_data_processor import *
+from scipy.spatial import distance
 
 
 class ClusteringEvaluator:
-    def __init__(self, X, y, model, n_cluster):
+    def __init__(self, X = None, y = None, model = None, n_cluster = None):
         self.X = X
         self.y = y
         self.model = model
         self.n_cluster = n_cluster
 
     def evaluate_clustering(self):
-
         # Calculate Silhouette Coefficient
         logging.info("Calculate silhouette_score...")
         start_time = time.time()
@@ -33,14 +34,9 @@ class ClusteringEvaluator:
         end_time = time.time()
         silhouette_time = end_time - start_time
 
-        # Calculate Rand Index
-        logging.info("Calculate rand_index...")
-
-
+        #Predict label again
         new_labels = []
-
         kmeans_clusters = self.model.predict(self.X)
-
         for cluster in range(self.n_cluster):
             mask = self.model.labels_ == cluster
             if len(self.y[mask]) == 0:
@@ -49,14 +45,29 @@ class ClusteringEvaluator:
                 continue
             most_common_label = np.bincount(self.y[mask]).argmax()
             new_labels.append(most_common_label)
-
         approx_label = [new_labels[c] for c in kmeans_clusters]
         
         
         print('Truth labels : ', np.unique(self.y, return_counts= True))
         print('Predicted labels: ', np.unique(approx_label, return_counts=True))
 
+
+        # Calculate Rand Index
+        logging.info("Calculate rand_index...")
+        start_time = time.time()
+        rand_index = metrics.adjusted_rand_score(self.y, approx_label)
+        end_time = time.time()
+        rand_index_time = end_time - start_time
+
+        # Calculate Sum of Squared Distance (SSD)
+        logging.info('Calculate Sum of Squared Distance (SSD)')
+        start_time = time.time()
+        ssd = self.model.inertia_
+        end_time = time.time()
+        ssd_time = end_time - start_time
+
         # compute contingency matrix (also called confusion matrix)
+        logging.info('Calculate purity score')
         start_time = time.time()
         contingency_matrix = metrics.cluster.contingency_matrix(self.y, approx_label)
         # calculate purity for each cluster
@@ -64,38 +75,121 @@ class ClusteringEvaluator:
         end_time = time.time()
         purity_time = end_time - start_time
 
-        start_time = time.time()
-        rand_index = metrics.adjusted_rand_score(self.y, approx_label)
-        end_time = time.time()
-        rand_index_time = end_time - start_time
-
-        # Calculate Sum of Squared Distance (SSD)
-        start_time = time.time()
-        ssd = self.model.inertia_
-        end_time = time.time()
-        ssd_time = end_time - start_time
-
         logging.info(f'Silhouette Coefficient: {silhouette:.2f} (Elapsed time: {silhouette_time:.2f} seconds)')
         logging.info(f'Rand Index: {rand_index:.2f} (Elapsed time: {rand_index_time:.2f} seconds)')
         logging.info(f'Sum of Squared Distance (SSD): {ssd:.2f} (Elapsed time: {ssd_time:.2f} seconds)')
         logging.info(f'Avg Purity of clustering: {np.mean(purity):.2f} (Elapsed time: {purity_time:.2f} seconds)')
 
+    # def DBSCAN_evaluate(self):
+        
+    #     # Calculate Silhouette Coefficient
+    #     logging.info("Calculate silhouette_score...")
+    #     start_time = time.time()
+    #     silhouette = metrics.silhouette_score(self.X, self.model.labels_)
+    #     end_time = time.time()
+    #     silhouette_time = end_time - start_time
 
-def propagate_labels(labeled_data, labeled_labels, unlabeled_data):
+    #     #Predict label again
 
-    config_path = './src/model_config/'+ args.phase_id + '/' + args.prob_id +'/cluster.json'
+    #     approx_label = dbscan_predict(self.model, self.X)
+    #     print('Truth labels : ', np.unique(self.y, return_counts= True))
+    #     print('Predicted labels: ', np.unique(approx_label, return_counts=True))
+
+    #     # Calculate Rand Index
+    #     logging.info("Calculate rand_index...")
+    #     start_time = time.time()
+    #     rand_index = metrics.adjusted_rand_score(self.y, approx_label)
+    #     end_time = time.time()
+    #     rand_index_time = end_time - start_time
+
+    #     # compute contingency matrix (also called confusion matrix)
+    #     logging.info('Calculate purity score')
+    #     start_time = time.time()
+    #     contingency_matrix = metrics.cluster.contingency_matrix(self.y, approx_label)
+    #     # calculate purity for each cluster
+    #     purity = np.amax(contingency_matrix, axis=0) / np.sum(contingency_matrix, axis=0)
+    #     end_time = time.time()
+    #     purity_time = end_time - start_time
+
+    #     logging.info(f'Silhouette Coefficient: {silhouette:.2f} (Elapsed time: {silhouette_time:.2f} seconds)')
+    #     logging.info(f'Rand Index: {rand_index:.2f} (Elapsed time: {rand_index_time:.2f} seconds)')
+    #     logging.info(f'Avg Purity of clustering: {np.mean(purity):.2f} (Elapsed time: {purity_time:.2f} seconds)')
+
+
+
+# Define a function to assign new data points to clusters
+# def dbscan_predict(dbscan_model, X_new, metric=distance.euclidean):
+#     # Result is noise by default
+#     y_new = np.ones(shape=len(X_new), dtype=int) #*-1
+
+#     # Iterate all input samples for a label
+#     for j, x_new in enumerate(X_new):
+#         # Find a core sample closer than EPS
+#         for i, x_core in enumerate(dbscan_model.components_):
+#             if metric(x_new, x_core) < dbscan_model.eps:
+#                 # Assign label of x_core to x_new
+#                 y_new[j] = dbscan_model.labels_[dbscan_model.core_sample_indices_[i]]
+#                 break
+
+#     return y_new
+
+# def prob2_propagate_labels(labeled_data, labeled_labels, unlabeled_data):
+#     #load config file
+#     config_path = './src/model_config/'+ prob_config.phase_id + '/' + prob_config.prob_id +'/cluster.json'
+#     with open(config_path, "r") as f:
+#         model_params = json.load(f)
+
+#     #define model
+#     algorithm = model_params["algorithm"]["name"] # 'k-means' or 'DBSCAN' or 'MiniBatchKMeans'
+#     logging.info(f"Use {algorithm} algorithm to labeling captured data")
+#     logging.info(f"Parameters: {model_params['dbscan']}")
+#     clusterer = DBSCAN(**model_params["dbscan"])
+
+#     #train model
+#     #Load if existed or train new model
+#     saved_model = f'./src/model_config/{prob_config.phase_id }/{prob_config.prob_id }/{algorithm}_{model_params["dbscan"]["eps"]}_{model_params["dbscan"]["min_samples"]}_model.pkl'
+#     if os.path.isfile(saved_model):
+#         logging.info("Oh existed trained model, nice!")
+#         clusterer = joblib.load(saved_model)
+#     else:
+#         logging.info("Ops, waiting for happiness :))")
+#         start_time = time.time()
+#         clusterer.fit(labeled_data)
+#         end_time = time.time()
+#         joblib.dump(clusterer, saved_model)
+#         logging.info(f'Elapsed time: {(end_time - start_time):.2f}')
+    
+#     print(np.unique(clusterer.labels_, return_counts=True))
+#     return
+
+#     #evaluate
+#     logging.info('Evaluate cluster model... ')
+#     evaluator = ClusteringEvaluator(X=labeled_data, y=labeled_labels, model=clusterer)
+#     evaluator.DBSCAN_evaluate()
+
+#     # Step 3: Propagate labels to the rest of the data
+
+#     logging.info("Labeling new data...")
+#     propagated_labels = dbscan_predict(clusterer, unlabeled_data)
+
+#     all_labels = np.concatenate((labeled_labels, propagated_labels), axis=0)
+#     # Merge the labeled and unlabeled data
+#     data = np.concatenate((labeled_data, unlabeled_data), axis=0)
+
+#     return data, all_labels, unlabeled_data, propagated_labels
+
+
+def prob1_propagate_labels(labeled_data, labeled_labels, unlabeled_data):
+
+    config_path = './src/model_config/'+ prob_config.phase_id + '/' + prob_config.prob_id +'/cluster.json'
     with open(config_path, "r") as f:
         model_params = json.load(f)
 
-    algorithm = model_params["algorithm"]["name"] # 'k-means' or 'DBSCAN' or 'MiniBatchKMeans'
+    algorithm = model_params["algorithm"]["name"] # 'k-means' or 'MiniBatchKMeans'
     logging.info(f"Use {algorithm} algorithm to labeling captured data")
 
-    if algorithm == 'DBSCAN':
-        # Step 2: Cluster the data using DBSCAN
-        logging.info(f"Parameters: {model_params['dbscan']}")
-        clusterer = DBSCAN(**model_params["dbscan"])
 
-    elif algorithm == 'k-means':
+    if algorithm == 'k-means':
         # Step 2: Cluster the data using k-means
         logging.info(f"Parameters: {model_params['k_means']}")
         clusterer = KMeans(**model_params["k_means"])
@@ -107,12 +201,9 @@ def propagate_labels(labeled_data, labeled_labels, unlabeled_data):
         clusterer = MiniBatchKMeans(**model_params["mini"])
         n_cluster = model_params["mini"]["n_clusters"]
 
-
-    
-
     logging.info("Fitting labeled data...")
 
-    saved_model = f'./src/model_config/{args.phase_id }/{args.prob_id }/{n_cluster}_{algorithm}_model.pkl'
+    saved_model = f'./src/model_config/{prob_config.phase_id }/{prob_config.prob_id }/{n_cluster}_{algorithm}_model.pkl'
     if os.path.isfile(saved_model):
         logging.info("Oh existed trained model, nice!")
         clusterer = joblib.load(saved_model)
@@ -127,15 +218,13 @@ def propagate_labels(labeled_data, labeled_labels, unlabeled_data):
     
 
     logging.info('Evaluate cluster model... ')
-    evaluator = ClusteringEvaluator(labeled_data, labeled_labels, clusterer, n_cluster)
+    evaluator = ClusteringEvaluator(X=labeled_data, y=labeled_labels, model=clusterer, n_cluster=n_cluster)
     evaluator.evaluate_clustering()
 
     # Step 3: Propagate labels to the rest of the data
     logging.info("Labeling new data...")
     new_labels = []
-
     kmeans_clusters = clusterer.predict(unlabeled_data)
-    
     for cluster in range(n_cluster):
         mask = clusterer.labels_ == cluster
         if len(labeled_labels[mask]) == 0:
@@ -147,7 +236,7 @@ def propagate_labels(labeled_data, labeled_labels, unlabeled_data):
     # Get the centroids of each cluster
     centroids = clusterer.cluster_centers_
     # Set the batch size
-    batch_size = 1000
+    batch_size = 5000
     # Initialize an array to store the nearest cluster for each cluster
     nearest_clusters = np.empty(centroids.shape[0], dtype=int)
     # Calculate the distances between the centroids in batches
@@ -255,9 +344,14 @@ def label_captured_data(prob_config: ProblemConfig):
 
     logging.info("Initialize and fit the clustering model")
 
-   
-    total_data, total_label, captured_data, approx_label = propagate_labels(labeled_data, labeled_labels, unlabeled_data)
-    # print(np.unique(approx_label))
+    # if prob_config.prob_id == 'prob1':
+    #     total_data, total_label, captured_data, approx_label = prob1_propagate_labels(labeled_data, labeled_labels, unlabeled_data)
+    #     # print(np.unique(approx_label))
+    # else:
+    #     total_data, total_label, captured_data, approx_label = prob2_propagate_labels(labeled_data, labeled_labels, unlabeled_data)
+    
+    total_data, total_label, captured_data, approx_label = prob1_propagate_labels(labeled_data, labeled_labels, unlabeled_data)
+
 
     logging.info("Saving new data...")
     captured_x = pd.DataFrame(captured_data, columns = columns)
